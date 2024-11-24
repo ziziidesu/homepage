9033226d3a154da12e77acb66ebf59407b21c7d5
ziziidesu <scratch.ziziidesu.scratch@gmail.com>
2024-11-24 10:48:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 静的ファイルの提供
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ルートハンドラ
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
